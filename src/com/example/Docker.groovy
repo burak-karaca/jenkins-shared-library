c5ef9fcd20301dabd9bf9ab86ccba9dd5137ca70
Karaca <burak.karaca@nokia.com>
2022-06-07 14:47:26 +0200

    call docker build with source code

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,18 +1,20 @@
 #!/usr/bin/env groovy
 package com.example
 
-class Docker implements Serializable{
+class Docker implements Serializable {
 
     def script
-    Docker(script){
+
+    Docker(script) {
         this.script = script
     }
 
-    def buildDockerImage(String imageName){
+    def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
-        script.withCredentials([script.usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        script.withCredentials([script.usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "docker build -t $imageName ."
             script.sh "echo $script.PASS | docker login -u ${script.USER} --password-stdin"
             script.sh "docker push $imageName"
+        }
     }
 }
