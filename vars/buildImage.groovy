ce674a6b16ca0c56d4f7b091f3a75347020970a0
Burak Karaca <cloudofburak@gmail.com>
2022-10-21 15:26:06 +0200

    some changes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -4,5 +4,10 @@ import com.example.Docker
 def call(String imageName) {
     return new Docker(this).buildDockerImage(imageName)
 
-
+    // echo "building the docker image"
+    // withCredentials([usernamePassword(credentialsId: 'docker-burak', passwordVariable: 'PASS', usernameVariable: "USER")]){
+    //     sh "docker build -t $imageName ."
+    //     sh "echo $PASS | docker login -u $USER --password-stdin"
+    //     sh "docker push $imageName"
+    // }
 }
