b3d7426c49bf0eb83de5858c69a6ed4cb8b41cdc
Burak Karaca <cloudofburak@gmail.com>
2022-10-21 15:29:11 +0200

    some changes

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -3,13 +3,11 @@ package com.example
 
 class Docker implements Serializable  {
     def script
-
     Docker(script){
         this.script
     }
-
     def buildDockerImage(String imageName){
-        script.echo "building the docker image"
+        script.sh "echo 'building the docker image'"
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-burak', passwordVariable: 'PASS', usernameVariable: "USER")]){
             script.sh "docker build -t $imageName ."
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
